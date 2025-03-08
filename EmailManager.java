1478878931884af2037ac024905c258500255263
Neeraj2005-github <143681033+Neeraj2005-github@users.noreply.github.com>
2025-03-08 18:25:40 +0530

    Create EmailManager.java

---
+++ EmailManager.java
@@ -0,0 +1,31 @@
+package klu.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailManager {
+	
+	@Autowired
+	JavaMailSender JMS;
+	SimpleMailMessage mailMessage=new SimpleMailMessage();
+	public String sendMail(String toMail,String subject,String message) {
+		try {
+		mailMessage.setFrom("gurushaileshmopuri@gmail.com");
+		mailMessage.setTo(toMail);
+		mailMessage.setSubject(subject);
+		mailMessage.setText(message);
+		JMS.send(mailMessage);
+		return "200::password sent to registered mail id";
+		}catch(Exception e) {
+			return "401::"+e.getMessage();
+			
+		}
+		
+		
+	}
+	
+
+}
